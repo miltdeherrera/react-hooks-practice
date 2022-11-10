cb69b20f3c4bab4107499dc606141ad351c6aaf2
Milt Deherrera <miltdeherrera@gmail.com>
2022-11-10 13:04:59 -0800

    add sidebar

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
@@ -6,7 +6,9 @@ import './ProductView.css'
 function ProductView({ products }) {
 
   // TODO: Replace with state variable
-  const sideOpen = true;
+  // const sideOpen = true;
+const [sideOpen, setSideOpen] = useState(true);
+const [selectedProduct, setSelectedProduct] = useState('');
 
   return (
     <div className="product-view">
@@ -17,7 +19,7 @@ function ProductView({ products }) {
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              onClick={() => setSelectedProduct(item)}
             />
           )}
         </div>
@@ -25,11 +27,11 @@ function ProductView({ products }) {
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
           <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
+               onClick={() => setSideOpen(!sideOpen)}>
             {sideOpen ? '>' : '<'}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails product={selectedProduct} visible={sideOpen} />
       </div>
     </div>
   );
